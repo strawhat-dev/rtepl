7cd97b19de9c5b02db18254b339b879a02b11b39
strawhat-dev <kn37.dev@gmail.com>
2023-06-09 01:34:28 -0700

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,8 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
+  rules: { 'no-empty': 0 },
   extends: ['eslint:recommended'],
   env: { es2022: true, node: true },
-  parserOptions: { sourceType: 'module', ecmaVersion: 2022 },
+  parserOptions: { sourceType: 'module', ecmaVersion: 'latest' },
 };
